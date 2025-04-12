48d8dcb4f6b217e23dc2989919056a2bf886c7f2
Marcos Tavares <mjtdes.md@gmail.com>
2025-04-12 16:33:40 -0300

    feat: Create the initial structure for the BoardView
    This view will hold the Kanban board, with the columns and the tasks

---
+++ src/views/BoardView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+import { useKanbanStore } from '../store/kanban'
+
+const kanban = useKanbanStore()
+
+function tasksByColumn(name) {
+    return kanban.tasks.filter(task => task.columnName === name)
+}
+</script>
+
+<template>
+    <div>
+        <header>
+            <h1>Kanban Board</h1>
+            <button>+ Add new task</button>
+        </header>
+
+        <main>
+            
+        </main>
+    </div>
+</template>
