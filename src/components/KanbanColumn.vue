ecb42b4c2a33a41460612afb4a4b6e8e813483be
Marcos Tavares <mjtdes.md@gmail.com>
2025-04-12 16:48:32 -0300

    feat: Create the KanbanColumn component for testing in the BoardView

--- src/components/KanbanColumn.vue
+++ src/components/KanbanColumn.vue
@@ -1,7 +1,39 @@
 <script setup>
+import { onMounted, ref } from 'vue'
+import Sortable from 'sortablejs'
+import { useKanbanStore } from '../store/kanban'
+import KanbanPostIt from './KanbanPostIt.vue'
 
+const props = defineProps({
+    column: Object,
+    tasks: Array
+})
+
+const taskList = ref([])
+const kanban = useKanbanStore()
+
+onMounted(() => {
+    Sortable.create(taskList.value, {
+        group: 'tasks',
+        animation: 150,
+        onAdd(event) {
+            const taskId = Number(event.item.dataset.id)
+            kanban.moveTask(taskId, props.column.name)
+        }
+    })
+})
 </script>
 
 <template>
-    
+    <div class="kanban-column" :style="{ backgroundColor: column.color }" ref="container">
+        <h2>{{ column.name }}</h2>
+
+        <div class="task-list" ref="taskList">
+            <KanbanPostIt
+                v-for="task in tasks"
+                :key="task.id"
+                :task="task"
+            />
+        </div>
+    </div>
 </template>
