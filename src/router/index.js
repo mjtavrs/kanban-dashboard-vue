b3d299db1e4513080011b02938bd7a88905550a1
Marcos Tavares <mjtdes.md@gmail.com>
2025-04-12 16:55:05 -0300

    refactor: Uncomment the board route inside the router for testing

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import BoardView from '../views/BoardView.vue'
 
 const routes = [
     {
         path: '/', 
         component: HomeView
     },
-    // {
-    //     path: '/board',
-    //     component: BoardView
-    // }
+    {
+        path: '/board',
+        component: BoardView
+    }
 ]
 
 export const router = createRouter({
