0fa1e5a651fe942841b05931c75ab27f8bd4e511
Marcos Tavares <mjtdes.md@gmail.com>
2025-04-14 09:26:08 -0300

    refactor: Delete the configuration route from the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import ConfigKanbanView from '../views/ConfigKanbanView.vue'
 import BoardView from '../views/BoardView.vue'
 
 const routes = [
     {
         path: '/', 
         component: HomeView
-    },
-    {
-        path: '/configure', 
-        component: ConfigKanbanView
     },
     {
         path: '/board',
