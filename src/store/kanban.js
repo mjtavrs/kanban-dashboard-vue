adc57bc1576c77ba2b3efcfd4ff571564e3a8e03
Marcos Tavares <mjtdes.md@gmail.com>
2025-04-14 10:57:27 -0300

    feat: Add a description to each column of the board

--- src/store/kanban.js
+++ src/store/kanban.js
@@ -3,11 +3,31 @@ import { defineStore } from 'pinia'
 export const useKanbanStore = defineStore('kanban', {
     state: () => ({
         columns: [
-            { name: 'Backlog', color: 'gainsboro' },
-            { name: 'To-do', color: 'darkorange' },
-            { name: 'In Progress', color: 'lightblue' },
-            { name: 'Testing', color: 'plum' },
-            { name: 'Done', color: 'darkseagreen' }
+            { 
+                name: 'Backlog', 
+                color: 'gainsboro',
+                description: 'List of tasks not yet prioritized.'
+            },
+            { 
+                name: 'To-do', 
+                color: 'darkorange',
+                description: 'Tasks already prioritized and ready to be worked on.'
+            },
+            { 
+                name: 'In Progress', 
+                color: 'lightblue',
+                description: 'Tasks already in progress and being worked on.'
+            },
+            { 
+                name: 'Testing', 
+                color: 'plum',
+                description: 'Finished tasks that are being tested.'
+            },
+            { 
+                name: 'Done', 
+                color: 'darkseagreen',
+                description: 'Finish and approved tasks.'
+            }
         ],
         tasks: []
     }),
